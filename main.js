2a93ffc90954b5c7b4c4f10beedda3fe87b71db1
Mahdi891-MH <mahdihussaini891@gmail.com>
2025-12-12 10:29:46 +0430

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+var operation = " ";
+function get_value(val){
+    document.getElementById("calc_area").innerHTML +=  val;
+}
+function calculate(){
+    var number = document.getElementById("calc_area").innerHTML;
+    var numbers = number.split(operation);
+    if (operation == "+")
+        document.getElementById("calc_area").innerHTML = parseFloat(numbers[0])+parseFloat(numbers[1]);
+    else if (operation == "-")
+        document.getElementById("calc_area").innerHTML = parseFloat(numbers[0])-parseFloat(numbers[1]);
+    else if (operation == "x")
+        document.getElementById("calc_area").innerHTML = parseFloat(numbers[0])*parseFloat(numbers[1]);
+    else if (operation == "÷")
+        document.getElementById("calc_area").innerHTML = parseFloat(numbers[0])/parseFloat(numbers[1]) 
+    else
+        document.getElementById("calc_area").innerHTML = number;
+    operation = "";
+}
+function def_operation(oprn){
+    operation = oprn;
+}
+function dlt(){
+    var value = document.getElementById("calc_area").innerHTML;
+    var index = value.length-1;
+    document.getElementById("calc_area").innerHTML = value.slice(0, -1);
+}
